d174f2e23bcd7bb6da47867f5f608446708b6a2f
Hussain <hussain_miya@hotmail.com>
2021-12-10 14:07:39 -0500

    update

--- ADC_Test.py
+++ ADC_Test.py
@@ -63,8 +63,15 @@ setPointIDevice = DigitalInputDevice(PIN_SET_POINT_CMPR)
 ctrlTempIDevice = DigitalInputDevice(PIN_CTRL_SNSR_CMPR)
 alarmODevice = PWMOutputDevice(PIN_ALARM_PWM, frequency=10000)
 alarmODevice.value = 0.8
+
+def snoozeHandler(self):
+    print("PRESSED!")
+
+snoozeButton = gpiozero.Button(PIN_SNOOZE_BTN) 
+snoozeButton.when_pressed = snoozeHandler
+
 while(1):
-    time.sleep(1)
+    gpiozero.pause()
     #print("FINAL:", read_ADC_sensors_binary())
     #comparator_test()
     #alarmODevice.on()

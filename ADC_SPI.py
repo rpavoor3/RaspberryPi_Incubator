01bbdc6b93ab561461f5f2aeab1bc5dc8cf9b1c0
Hussain <hussain_miya@hotmail.com>
2022-01-27 16:56:18 -0500

    f

--- ADC_SPI.py
+++ ADC_SPI.py
@@ -4,7 +4,8 @@ from time import sleep
 adc = MCP3002(channel=0)
 
 while(1):
-    data = 100 * (adc.value * 5) - 50
+    # data = 100 * (adc.value * 5) - 50
+    data = adc.value * 5
     print(data)
     sleep(0.25)
 

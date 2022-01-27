9ad4a6138e15e5a284f72be49c895581dfe89caa
Hussain <hussain_miya@hotmail.com>
2022-01-27 16:30:42 -0500

    f

--- ADC_SPI.py
+++ ADC_SPI.py
@@ -1,10 +1,10 @@
 from gpiozero import MCP3002
 from time import sleep
 
-adc = MCP3002(channel=0)
-
+adc = MCP3002(channel=1)
 
 while(1):
-    print(adc.value * 5)
+    data = adc.value
+    print(data)
     sleep(0.5)
 
